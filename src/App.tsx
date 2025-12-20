24fc00d00cb1dfb08d19c7e5c1f03cfc93694840
CrommVardek <a@a.a>
2025-12-20 15:24:56 +0100

    Adding Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Header } from './components/Header'
 import { CountriesNetworkPage } from './components/pages/country-alliances/CountriesAlliancesPage'
 
 function App() {
 
   return (
     <>
+      <Header />
       <CountriesNetworkPage />
     </>
   )
