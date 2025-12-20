503866fdf86cb0309dfefd820227a3ee359e210b
CrommVardek <a@a.a>
2025-12-20 15:35:13 +0100

    Adding Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <span className="header-title">War Era Explorer</span>
+        </header>
+    );
+}
